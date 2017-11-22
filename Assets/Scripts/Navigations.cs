d0cf629dfc4ec6a1b9a7f1a0821d85e6f33d6e29
Master-Owl <cheesytrevor@gmail.com>
2017-11-21 21:25:04 -0700

    Added quit button

---
+++ Assets/Scripts/Navigations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigations : MonoBehaviour {
+
+	public void LoadSceneByIndex(int index) {
+		SceneManager.LoadScene(index);
+	}
+
+	public void Quit() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+}
